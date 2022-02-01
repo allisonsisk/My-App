2a9d69140e6a22ab6cd188656cef96562392dd64
allison sisk <allisonsisk04@gmail.com>
2022-02-01 12:01:11 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,31 @@
+import Subsonic
+import SwiftUI
+
+struct ContentView: View {
+    let names = ["1", "2", "3"]
+    let column = [
+        GridItem(.adaptive(minimum:250))
+    ]
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: column){
+                    ForEach(names, id: \.self){ name in
+                        Button{
+                            play(sound: "FullSizeRender.MOV.mp3")
+                            print("\(name) was tapped")
+                        } label: {
+                            Image(name)
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(25)
+                                .padding(.horizontal)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Minecraft 2.0")
+        }
+        .navigationViewStyle(.stack)
+    }
+}
